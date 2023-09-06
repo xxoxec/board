5e0e673539e6f1912c53c853fc5434983d617afd
xxo <xxoxec@gmail.com>
2023-09-06 16:08:18 +0900

    feat: AuditingFields 추출 ( @MappedSuperclass 이용, 상속으로 추출 그 외 @DataTimeFormat 요소 추가 및 jpa 옵션 개선)

--- src/main/java/com/example/board/domain/ArticleComment.java
+++ src/main/java/com/example/board/domain/ArticleComment.java
@@ -4,12 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
@@ -20,22 +14,13 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Setter @ManyToOne(optional = false) private Article article;
     @Setter @Column(nullable = false, length = 500) private String content;
 
-
-    @CreatedDate
-    @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy
-    @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate
-    @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy
-    @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
 
     protected ArticleComment() {}
 
